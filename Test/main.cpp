20b1ea63a309436077b95ffade1dbce33980774b
Krzysztof Szczerbowski <krzysiek@rincewind-4.local>
2018-10-10 23:33:09 +0200

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Krzysztof Szczerbowski on 10/10/2018.
+//  Copyright © 2018 Krzysztof Szczerbowski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
